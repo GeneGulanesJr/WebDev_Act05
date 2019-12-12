6de1d27212022d64a937e9034cef79b796e18154
Gene Gulanes Jr <ggulanes_180000000405@uic.edu.ph>
2019-12-12 11:25:50 +0800

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,5 +1,53 @@
 iisApp.controller('login', function ($scope, $http) {
 
+
+	$scope.login_attempts = $('#loginAttempts').val();
+	$scope.login_penalty = $('#loginPenalty').val();
+	$scope.login_current_datetime = $('#currentDateTime').val();
+
+	$scope.runTimer = function(time, format){
+		let timerInterval
+		Swal.fire({
+			title: 'Reached the maximum attempt',
+			html: 'You can login in <strong><span id="swal-minutes"></span></strong> minutes <strong><span id="swal-seconds"></span></strong> seconds.',
+			type: 'info',
+			timer: time,
+  			allowOutsideClick: () => false,
+			onOpen: () => {
+				Swal.showLoading()
+				timerInterval = setInterval(() => {
+				    const content = Swal.getContent()
+				    const $ = content.querySelector.bind(content)
+				    Swal.getContent().querySelector('#swal-minutes').textContent = Math.floor(Swal.getTimerLeft() / 60000)
+				    Swal.getContent().querySelector('#swal-seconds').textContent = ((Swal.getTimerLeft() % 60000) / 1000).toFixed(0)
+				}, 500)
+			},
+			onClose: () => {
+				clearInterval(timerInterval)
+			}
+		}).then((result) => {
+			if (result.dismiss === Swal.DismissReason.timer) {
+				$scope.login_attempts = 0;
+				$scope.login_penalty = "";
+				$scope.login_current_datetime = "";
+			}
+		});
+	};
+
+	$scope.checkPenalty = function(penalty,current,format){
+		if (Date.parse(penalty)) {
+			penalty = new Date(penalty);
+			current = new Date(current);
+
+			if(current < penalty){
+				$scope.runTimer((penalty.getTime()-current.getTime()),format);
+			}
+			else{
+				$scope.login_attempts = 0;
+			}
+		}
+	};
+
 	$scope.Login = function(record){
 		if($scope.login_attempts < 5){
 			var dsp_attempts = $scope.login_attempts;
@@ -14,16 +62,38 @@ iisApp.controller('login', function ($scope, $http) {
 	  			allowOutsideClick: () => !swal.isLoading(),
 				preConfirm: () => {
 					return $http({
+								method: 'GET',
+								url: url + '?fa=login.login_process',
+								params: {
+								    'username': record.username,
+								    'password': record.password
+								}
 							})
 							.then(function(response) {
-								if(a=b) {
+								if(response.data.result_id > 0) {
+									window.location.href = url + '?fa=home.index';
+								}
+								else {
+									$scope.login_attempts = response.data.attempts;
+									$scope.login_penalty = response.data.penalty;
+									$scope.login_current_datetime = response.data.current;
+
+									swal.insertQueueStep({
+										type: 'error',
+										title: 'Incorrect username or password!',
+										confirmButtonText: 'Try Again',
+										onClose: () => {
+											if(Date.parse($scope.login_penalty)){
+												$scope.checkPenalty($scope.login_penalty, $scope.login_current_datetime, 'queue');
+											}
+										}
+									});
 								}
 							})
 							.catch(function(response) {
 								swal.insertQueueStep({
 									type: 'success',
 									title: 'Login Successful'
-
 								})
 								console.log(response);
 							})
@@ -34,16 +104,16 @@ iisApp.controller('login', function ($scope, $http) {
 				}
 			}]);
 		}
+		else{
+			$scope.checkPenalty($scope.login_penalty,$scope.login_current_datetime,'');
+		}
 
 
 
 	};
 
 
+	$scope.checkPenalty($scope.login_penalty,$scope.login_current_datetime,'');
 
-	window.history.pushState(null, "", window.location.href);        
-    window.onpopstate = function() {
-        window.history.pushState(null, "", window.location.href);
-    };
 
 });
